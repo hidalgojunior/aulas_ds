ac3c7bf9e50fe70bf80f72c42df2f30cb9f2d04a
arnaldo@hidalgojunior.com.br <hidalgojunior>
2021-02-17 02:21:23 -0300

    Iniciando a resposta dos Exercícios da Aula03 - Desenvolvimento de Sistemas.

---
+++ aula03/Exercicio01.java
@@ -0,0 +1,10 @@
+public class Exercicio01{
+    public static void main (String [] args){
+        float base, altura, area;
+
+        base = 4;
+        altura = 3;
+        area = base * altura;
+        System.out.println("A área do retangulo é "+area);
+    }
+}
